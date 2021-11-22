fd583b29901cfadd5ceee00f3d6fa6815769c61a
DjTer3a <44501304+DjTer3a@users.noreply.github.com>
2021-11-22 17:09:28 +0300

    last try

--- i18n.js
+++ i18n.js
@@ -1,9 +1,9 @@
-//import the default class contructor from the package
-const NextI18Next = require('next-i18next').default
-const path = require('path')
-module.exports = new NextI18Next({
-  defaultLanguage:'en',
-  otherLanguages: ['ar'],
-  otherLanguages: ['tr'],
-  localePath: path.resolve('./public/static/locales')
-})
+// //import the default class contructor from the package
+// const NextI18Next = require('next-i18next').default
+// const path = require('path')
+// module.exports = new NextI18Next({
+//   defaultLanguage:'en',
+//   otherLanguages: ['ar'],
+//   otherLanguages: ['tr'],
+//   localePath: path.resolve('./public/static/locales')
+// })
