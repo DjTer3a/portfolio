f0dc4093551552d7f7f681a242d6c5d650f8858f
Mostafa Higazy <44501304+DjTer3a@users.noreply.github.com>
2022-10-21 23:24:34 +0300

    website overhaul

--- i18n.js
+++
@@ -1,9 +0,0 @@
-// //import the default class contructor from the package
-// const NextI18Next = require('next-i18next').default
-// const path = require('path')
-// module.exports = new NextI18Next({
-//   defaultLanguage:'en',
-//   otherLanguages: ['ar'],
-//   otherLanguages: ['tr'],
-//   localePath: path.resolve('./public/static/locales')
-// })
