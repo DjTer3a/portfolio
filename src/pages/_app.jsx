14d0f888e5965daf34da78f44ddb02293dbd6595
Mostafa Higazy <44501304+DjTer3a@users.noreply.github.com>
2022-11-09 02:50:34 +0300

    adding google analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,6 +5,7 @@ import { Header } from '@/components/Header'
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
+import Script from 'next/script'
 
 function usePrevious(value) {
   let ref = useRef()
@@ -18,9 +19,24 @@ function usePrevious(value) {
 
 export default function App({ Component, pageProps, router }) {
   let previousPathname = usePrevious(router.pathname)
-
+  let scriptSrc = `"https://www.googletagmanager.com/gtag/js?id="+${process.env.NEXT_PUBLIC_GOOGLE_API}`
   return (
     <>
+    <Script strategy="afterInteractive" src={scriptSrc}/>
+    <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_API}', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+    />
       <div className="fixed inset-0 flex justify-center sm:px-8">
         <div className="flex w-full max-w-7xl lg:px-8">
           <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
